3cb6d04e12071c11dcbd1bedf12cca4e741bfff3
Ireisme <ireisme@github>
2013-06-08 22:07:02 -0400

    Re-worked angularjs front-end almost entirely.

--- ang/app/js/app.js
+++ ang/app/js/app.js
@@ -2,8 +2,8 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('nTweetStreamer', ['ui.bootstrap','nTweetStreamer.filters', 'nTweetStreamer.services', 'nTweetStreamer.directives', 'nTweetStreamer.controllers']).
+angular.module('nTweetStreamer', ['ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/streams', {templateUrl: 'partials/streams.html', controller: 'StreamsCtrl'});
+    $routeProvider.when('/streams', {templateUrl: 'partials/streams.html', controller: StreamsCtrl});
     $routeProvider.otherwise({redirectTo: '/streams'});
 }]);
--- ang/app/js/controllers.js
+++ ang/app/js/controllers.js
@@ -1,11 +1,73 @@
 'use strict';
 
 /* Controllers */
+function StreamsCtrl($scope, $http) {
+/*	$scope.streams = [
+		{
+			_id: "First Stream",
+			query: "track: bottoms",
+			tweetCount: "800"
+		},
+		{
+			_id: "Second Stream",
+			query: "\"locations\": \"-122.75,36.8,-121.75,37.8\"",
+			tweetCount: "800"
+		}
+	];*/
 
-angular.module('nTweetStreamer.controllers', []).
-  controller('StreamsCtrl', [function($scope) {
+	$scope.streamAction = function(stream){
+		if(stream.status === "Stopped")
+		{
+			$http.post("http://localhost:3000/streamcontrol/start/" + stream._id);
+			stream.status = "Running";
+			stream.action = "Stop";
+		}
+		else if(stream.status === "Running")
+		{
+			$http.post("http://localhost:3000/streamcontrol/stop/" + stream._id);
+			stream.status = "Stopped";
+			stream.action = "Start";
+		}
+	};
 
-}])
-  .controller('MyCtrl2', [function() {
+	$scope.getStreams = function(){
+		$http.get("http://localhost:3000/streams").success(function(data){
+			$scope.streams = $scope.formatStreams(data);
+		});
+	};
 
-}]);
+	$scope.addStream = function(stream){
+		$http.post("http://localhost:3000/streams", stream).success(function(data){
+			$scope.getStreams();
+		});
+	};
+
+	$scope.addStreamOpen = function(){
+		$scope.addedStream = {};
+		$scope.addStreamModal = true;
+	};
+
+	$scope.addStreamClose = function(){
+		$scope.addedStream.query = { track: $scope.addedStream.query };
+		$scope.addStream($scope.addedStream);
+		$scope.addStreamModal = false;
+	};
+
+	$scope.addStreamCancel = function(){
+		$scope.addedStream = {};
+		$scope.addStreamModal = false;
+	};
+
+	$scope.formatStreams = function(streams){
+		streams.forEach(function(stream){
+			if(stream.status === "Stopped")
+				stream.action = "Start";
+			else if(stream.status === "Running")
+				stream.action = "Stop";
+		});
+
+		return streams;
+	};
+
+	$scope.getStreams();
+}
