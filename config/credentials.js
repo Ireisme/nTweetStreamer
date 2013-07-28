bc91375b7b5aca5653f7189646450c43fcd03c05
ireisme <ireisme@gmail.com>
2013-07-28 18:48:34 -0400

    Re-added credentials and gave it default values.

---
+++ config/credentials.js
@@ -0,0 +1,10 @@
+var credentials = [
+	{
+		consumer_key: 'Your Consumer Key Here',
+		consumer_secret: 'Your Consumer Secret Here',
+		access_token_key: 'You Access Token Here',
+		access_token_secret: 'Your Access Token Secret Here'
+	}
+];
+
+module.exports = credentials;
